7f614e038fa873afc57d3880524ea29a2249ecdf
Rushikengar8686 <kengar.rushi@gmail.com>
2024-04-07 22:16:40 +0530

    Save changes

--- src/Emp_Pages/CreateEmp.js
+++ src/Emp_Pages/CreateEmp.js
@@ -2,11 +2,55 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 const CreateEmp = () => {
     const [getAllEmpList,setGetAllEmpList] = useState([]);
+    const [createEmployee,setCreateEmployee] = useState({
+        "empId": 0,
+        "empName": " ",
+        "empContactNo": " ",
+        "empAltContactNo": "",
+        "empEmail": "",
+        "addressLine1": " ",
+        "addressLine2": " ",
+        "pincode": " ",
+        "city": " ",
+        "state": " ",
+        "bankName": " ",
+        "ifsc": " ",
+        "accountNo": " ",
+        "bankBranch": " ",
+        "salary": 0
+      })
+      
     const getUrl = "https://onlinetestapi.gerasim.in/api/TeamSync/";
+
     const getAllEmp = async () =>{
         const result = await axios.get(`${getUrl}GetAllEmployee`);
         setGetAllEmpList(result.data.data);
     }
+    const getCreateEmployee = (event,key) =>{   
+        setCreateEmployee(preObj =>({...preObj,[key]:event.target.value}));
+    }
+    const SaveEmp = async () =>{
+        const result = await axios.post(`${getUrl}CreateEmployee`,createEmployee);
+        if(result.data.data){
+            alert("Employee Save Success...");
+            getAllEmp();
+        }else{
+            alert(result.data.message);
+        }
+    }
+    const onEdit = (editinfo)=>{
+        setCreateEmployee(editinfo);
+    } 
+    const updateEmpInfo = async() =>{
+        const result = await axios.post(`${getUrl}UpdateEmployee`,createEmployee);
+        if(result.data.data){
+            alert("Employee Update Success...");
+            getAllEmp();
+        }else{
+            alert(result.data.message);
+        }
+    }
+
     useEffect (()=>{
         getAllEmp();
     },[])
@@ -30,16 +74,20 @@ const CreateEmp = () => {
                                                             <h6 className="card-title">{employee.empName}</h6>
                                                             <p className="card-text">
                                                                 <small>{employee.empContactNo}</small><br />
-                                                                <small>{employee.empEmail}</small>
-                                                                <small>{employee.addressLine1}</small>
-
-                                                            </p>
+                                                                <small>{employee.empEmail}</small><br />
+                                                                <small>{employee.addressLine1}</small><br />
+                                                                <small>{employee.state}</small>
+                                                             </p>
                                                             <p>
                                                                 <strong>{employee.bankBranch}</strong><br />
                                                                 <strong>{employee.salary}</strong>
                                                             </p>
-                                                            <button className='btn btn-secondary'>Edit</button>
-                                                        </div>
+                                                            <span className='m-1'>
+                                                            <button className='btn btn-secondary' onClick={()=>{onEdit(employee)}}>Edit</button>
+                                                            <button className='btn btn-primary' onClick={()=>{onEdit(employee)}}>Delete</button>
+                                                      
+                                                            </span>
+                                                          </div>
                                                     </div>
                                                 </div>
                                             )
@@ -58,65 +106,75 @@ const CreateEmp = () => {
                             <div className="row">
                                 <div className="col-6">
                                     <label htmlFor="">Emp Name</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.empName} className='form-control' onChange={(event)=>{getCreateEmployee(event,'empName')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">Emp Contact No</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.empContactNo} className='form-control' onChange={(event)=>{getCreateEmployee(event,'empContactNo')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">Emp AltContact No</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.empAltContactNo} className='form-control' onChange={(event)=>{getCreateEmployee(event,'empAltContactNo')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">Emp Email</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.empEmail} className='form-control' onChange={(event)=>{getCreateEmployee(event,'empEmail')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">Address Line1</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.addressLine1} className='form-control' onChange={(event)=>{getCreateEmployee(event,'addressLine1')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">Address Line2</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.addressLine2} className='form-control' onChange={(event)=>{getCreateEmployee(event,'addressLine2')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">Pincode</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.pincode} className='form-control' onChange={(event)=>{getCreateEmployee(event,'pincode')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">City</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.city} className='form-control' onChange={(event)=>{getCreateEmployee(event,'city')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">State</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.state} className='form-control' onChange={(event)=>{getCreateEmployee(event,'state')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">Bank Name</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.bankName} className='form-control' onChange={(event)=>{getCreateEmployee(event,'bankName')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">IFSC</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.ifsc} className='form-control' onChange={(event)=>{getCreateEmployee(event,'ifsc')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">Account No</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.accountNo} className='form-control' onChange={(event)=>{getCreateEmployee(event,'accountNo')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">Bank Branch</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.bankBranch} className='form-control' onChange={(event)=>{getCreateEmployee(event,'bankBranch')}}/>
                                 </div>
                                 <div className="col-6">
                                     <label htmlFor="">Salary</label>
-                                    <input type="text" className='form-control'/>
+                                    <input type="text" value={createEmployee.salary} className='form-control' onChange={(event)=>{getCreateEmployee(event,'salary')}}/>
                                 </div>
                             </div>
                             <div className="row">
-                                <div className="col-12 mt-4 text-center">
-                                    <button type="button" className='btn btn-success'>Save</button>
+                                {
+                                    createEmployee.empId == ' ' &&  <div className="col-12 mt-4 text-center">
+                                    <button type="button" className='btn btn-success' onClick={SaveEmp}>Save</button>
                                 </div>
+                                }
+                                {
+                                    createEmployee.empId != '' && <div className="col-12 mt-4 text-center">
+                                    <button type="button" className='btn btn-warning' onClick={updateEmpInfo}>Update</button>
+    
+                                    </div>
+                                }
+                               
+                                
                             </div>
                            
                         </div>
